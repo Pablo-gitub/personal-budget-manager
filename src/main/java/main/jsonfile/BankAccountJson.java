1c5bd0763cdf6c65a2089604d3a772b22d62a383
Paolo Pietrelli <paolo.pietrelli@studio.unibo.it>
2023-10-23 03:19:00 +0200

    adding boolean validation transaction method

--- src/main/java/main/jsonfile/BankAccountJson.java
+++ src/main/java/main/jsonfile/BankAccountJson.java
@@ -1,5 +1,8 @@
 package main.jsonfile;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
 
 public final class BankAccountJson {
@@ -39,6 +42,40 @@ public final class BankAccountJson {
 
     public void setTransactions(final List<TransactionJson> transactions) {
         this.transactions = transactions;
+    }
+
+    public boolean validTransaction(final TransactionJson transaction) {
+        // If the transaction amount is positive, it is always valid
+        if (transaction.getAmount() >= 0) {
+            return true;
+        }
+
+        // Convert transaction date and time strings to Date objects
+        final SimpleDateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy HH:mm");
+        Date transactionDateTime;
+        try {
+            transactionDateTime = dateFormat.parse(transaction.getDate() + " " + transaction.getTime());
+        } catch (ParseException e) {
+            e.printStackTrace();
+            return false;
+        }
+
+        // Calculate total amount of previous transactions
+        double previousTransactionsTotal = 0;
+        for (final TransactionJson previousTransaction : this.transactions) {
+            try {
+                final Date previousDateTime = dateFormat.parse(previousTransaction.getDate() + " " + previousTransaction.getTime());
+                if (previousDateTime.before(transactionDateTime)) {
+                    previousTransactionsTotal += previousTransaction.getAmount();
+                }
+            } catch (ParseException e) {
+                e.printStackTrace();
+                return false;
+            }
+        }
+
+        // Check if the total of previous transactions is higher than the input transaction's amount
+        return previousTransactionsTotal > Math.abs(transaction.getAmount());
     }
 
 }
--- src/main/java/main/jsonfile/MoneyboxAccountJson.java
+++ src/main/java/main/jsonfile/MoneyboxAccountJson.java
@@ -1,5 +1,8 @@
 package main.jsonfile;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -49,6 +52,40 @@ public final class MoneyboxAccountJson {
             }
         }
         this.totalAmount = amount;
+    }
+    
+    public boolean validTransaction(final TransactionJson transaction) {
+        // If the transaction amount is positive, it is always valid
+        if (transaction.getAmount() >= 0) {
+            return true;
+        }
+
+        // Convert transaction date and time strings to Date objects
+        final SimpleDateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy HH:mm");
+        Date transactionDateTime;
+        try {
+            transactionDateTime = dateFormat.parse(transaction.getDate() + " " + transaction.getTime());
+        } catch (ParseException e) {
+            e.printStackTrace();
+            return false;
+        }
+
+        // Calculate total amount of previous transactions with the same currency
+        double previousTransactionsTotal = 0;
+        for (final TransactionJson previousTransaction : this.transactions) {
+            try {
+                final Date previousDateTime = dateFormat.parse(previousTransaction.getDate() + " " + previousTransaction.getTime());
+                if (previousDateTime.before(transactionDateTime) && previousTransaction.getCurrency().equals(transaction.getCurrency())) {
+                    previousTransactionsTotal += previousTransaction.getAmount();
+                }
+            } catch (ParseException e) {
+                e.printStackTrace();
+                return false;
+            }
+        }
+
+        // Check if the total of previous transactions with the same currency is higher than the input transaction's amount
+        return previousTransactionsTotal > Math.abs(transaction.getAmount());
     }
 
 }
